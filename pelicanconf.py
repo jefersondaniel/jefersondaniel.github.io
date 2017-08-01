e056ea31cb48cdc2fc6e6241ee474b85cea6da82
Jeferson Daniel <contato@jefersondaniel.com>
2017-07-31 21:39:08 -0300

    Updating pelican

--- pelicanconf.py
+++ pelicanconf.py
@@ -15,7 +15,6 @@ PATH = 'content'
 TIMEZONE = 'America/Sao_Paulo'
 
 DEFAULT_LANG = u'pt'
-HTML_LANG = u'pt'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,10 +23,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 FEED_RSS = 'feed.xml'
-
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
 THEME = "themes/Just-Read"
